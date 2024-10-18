95b7e7ec32bb7f198b2e291370df2498f89c910a
DdosTesting321 <kingnayaknayak447@gmail.com>
2024-10-18 10:06:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7859157821:AAEsuRXB4vAEDLCVAKobqjgeHIEzuomCGHI'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [6663863353]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
